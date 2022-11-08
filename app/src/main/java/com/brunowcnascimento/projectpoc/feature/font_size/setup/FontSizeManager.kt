855baffa5a7f0b003a602ce56e424c8e7d796519
Bruno Nascimento <bruno.nascimento@g.globo>
2022-11-08 19:57:39 -0300

    ajuste no FontSizeManager.kt

--- app/src/main/java/com/brunowcnascimento/projectpoc/feature/font_size/setup/FontSizeManager.kt
+++ app/src/main/java/com/brunowcnascimento/projectpoc/feature/font_size/setup/FontSizeManager.kt
@@ -5,8 +5,7 @@ import android.content.SharedPreferences
 class FontSizeManager(private val prefs: SharedPreferences) {
 
     private val unsetFontSizeValue = -1f
-    private val floatList = arrayListOf(0.7f, 1.0f, 1.3f, 1.6f)
-    var fontSizeSystem: Float = 1.0f
+    var fontSizeSystem: Float = FontSize.DEFAULT
 
     var fontSize: Float
         get() {
@@ -14,7 +13,7 @@ class FontSizeManager(private val prefs: SharedPreferences) {
             return if (scale == unsetFontSizeValue) {
                 1.0f
             } else {
-                floatList.first { fontSize -> fontSize == scale }
+                FontSize.fontSizeList.first { fontSize -> fontSize == scale }
             }
         }
         set(fontSize) {
