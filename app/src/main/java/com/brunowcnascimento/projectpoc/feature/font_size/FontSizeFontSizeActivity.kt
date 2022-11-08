a81dd8473c240b0c473f8547d6d31de1add84ed4
Bruno Nascimento <bruno.nascimento@g.globo>
2022-11-08 19:52:25 -0300

    Melhorando o codigo e organizando codigos espalhados

--- app/src/main/java/com/brunowcnascimento/projectpoc/feature/font_size/FontSizeFontSizeActivity.kt
+++ app/src/main/java/com/brunowcnascimento/projectpoc/feature/font_size/FontSizeFontSizeActivity.kt
@@ -9,6 +9,7 @@ import android.widget.SeekBar.OnSeekBarChangeListener
 import com.brunowcnascimento.projectpoc.utils.common.CommonGenericActivity
 import com.brunowcnascimento.projectpoc.databinding.ActivityFontSizeBinding
 import com.brunowcnascimento.projectpoc.feature.font_size.setup.FontSize
+import com.brunowcnascimento.projectpoc.feature.font_size.setup.FontSize.getFontSizeByPosition
 
 class FontSizeFontSizeActivity : CommonGenericActivity() {
 
@@ -18,9 +19,10 @@ class FontSizeFontSizeActivity : CommonGenericActivity() {
     private val prefsProgress by lazy { getProgressPrefs() }
 
     private val switchIsEnabled get() = prefsSwitch.getBoolean(PREFS_SWITCH_IS_CHECKED, false)
-    private val progressIsPosition get() = prefsProgress.getInt(PREFS_PROGRESS_IS_POSITION, POSITION_DEFAULT)
+    private val currentPosition get() = prefsProgress.getInt(PREFS_PROGRESS_IS_POSITION, POSITION_DEFAULT)
+    private val currentFontSize get() = FontSize.fontSizeList.getFontSizeByPosition(currentPosition)
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFontSizeBinding.inflate(layoutInflater)
         val safeBinding = binding ?: return
@@ -34,17 +36,17 @@ class FontSizeFontSizeActivity : CommonGenericActivity() {
         binding?.apply {
             setupSeekBar()
             setupSwitch()
-            setEnabledSeekBar()
+            isEnabledSeekBar()
             setVisibilityText()
         }
     }
 
     private fun setVisibilityText() {
-        binding?.textFontSize?.text = if(switchIsEnabled) {
-                getFontSize(progressIsPosition).toString()
-            } else {
-                fontSizeManager?.fontSizeSystem.toString()
-            }
+        binding?.textFontSize?.text = if (switchIsEnabled) {
+            currentFontSize.toString()
+        } else {
+            fontSizeManager?.fontSizeSystem.toString()
+        }
     }
 
     private fun setupSwitch() {
@@ -55,9 +57,7 @@ class FontSizeFontSizeActivity : CommonGenericActivity() {
                     prefsSwitch.edit()
                         .putBoolean(PREFS_SWITCH_IS_CHECKED, isChecked)
                         .apply()
-
-                    setEnabledSeekBar()
-                    if(!isChecked) updateFontSizeSystem() else getFontSizeByProgress(progressIsPosition)
+                    verifySwitchIsEnabled()
                     getToast("Switch $isChecked").show()
                 }
 
@@ -65,15 +65,9 @@ class FontSizeFontSizeActivity : CommonGenericActivity() {
         }
     }
 
-    private fun setEnabledSeekBar() {
-        binding?.apply {
-            seekbarFont.isEnabled = switchIsEnabled
-        }
-    }
-
     private fun setupSeekBar() {
         binding?.apply {
-            seekbarFont.progress = progressIsPosition
+            seekbarFont.progress = currentPosition
             seekbarFont.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
                 override fun onProgressChanged(seekBar: SeekBar?, progress: Int, p2: Boolean) {
                     prefsProgress.edit()
@@ -86,46 +80,28 @@ class FontSizeFontSizeActivity : CommonGenericActivity() {
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                    getFontSizeByProgress(progressIsPosition)
+                    updateFontSize(currentFontSize)
                 }
             })
         }
     }
 
-    private fun getFontSize(progress: Int): Float {
-        return when(progress) {
-            0 -> 0.7f
-            1 -> 1.0f
-            2 -> 1.3f
-            3 -> 1.6f
-            else -> 1.0f
-        }
+    private fun isEnabledSeekBar() {
+        binding?.seekbarFont?.isEnabled = switchIsEnabled
     }
 
-    private fun getFontSizeByProgress(progress: Int) {
-        updateFontSize(getFontSize(progress))
+    private fun verifySwitchIsEnabled() {
+        if (!switchIsEnabled) updateFontSize() else updateFontSize(currentFontSize)
     }
 
-    private fun updateFontSize(fontSize: Float) {
+    private fun updateFontSize(
+        fontSize: Float? = fontSizeManager?.fontSizeSystem ?: FontSize.DEFAULT
+    ) {
+        val safeFontSize = fontSize ?: FontSize.DEFAULT
         binding?.textFontSize?.text = fontSize.toString()
-        fontSizeManager?.fontSize = fontSize
+        fontSizeManager?.fontSize = safeFontSize
         recreate()
     }
-
-    private fun updateFontSizeSystem() {
-        val fontSize = fontSizeManager?.fontSizeSystem ?: 1.0f
-        binding?.textFontSize?.text = fontSize.toString()
-        fontSizeManager?.fontSize = fontSize
-        recreate()
-    }
-
-
-    private fun getSwitchIsEnablePrefs() =
-        prefsSwitch.getBoolean(PREFS_SWITCH_IS_CHECKED, false)
-
-    private fun getProgressIsPositionPrefs() =
-        prefsProgress.getInt(PREFS_PROGRESS_IS_POSITION, POSITION_DEFAULT)
-
 
     companion object {
         private const val POSITION_DEFAULT = 1
@@ -138,6 +114,9 @@ class FontSizeFontSizeActivity : CommonGenericActivity() {
         fun getIntent(context: Context) = Intent(context, FontSizeFontSizeActivity::class.java)
     }
 
-    private fun getSwitchPrefs(): SharedPreferences = getSharedPreferences(PREFS_SWITCH, Context.MODE_PRIVATE)
-    private fun getProgressPrefs(): SharedPreferences = getSharedPreferences(PREFS_PROGRESS, Context.MODE_PRIVATE)
+    private fun getSwitchPrefs(): SharedPreferences =
+        getSharedPreferences(PREFS_SWITCH, Context.MODE_PRIVATE)
+
+    private fun getProgressPrefs(): SharedPreferences =
+        getSharedPreferences(PREFS_PROGRESS, Context.MODE_PRIVATE)
 }
--- app/src/main/java/com/brunowcnascimento/projectpoc/feature/font_size/setup/FontSize.kt
+++ app/src/main/java/com/brunowcnascimento/projectpoc/feature/font_size/setup/FontSize.kt
@@ -1,8 +1,17 @@
 package com.brunowcnascimento.projectpoc.feature.font_size.setup
 
-enum class FontSize(val scale: Float) {
-    SMALL(0.7f),
-    DEFAULT(1.0f),
-    LARGE(1.3f),
-    LARGEST(1.6f)
+object FontSize {
+    const val SMALL = 0.7f
+    const val DEFAULT = 1.0f
+    const val LARGE = 1.3f
+    const val LARGEST = 1.6f
+
+    val fontSizeList = arrayListOf(
+        SMALL,
+        DEFAULT,
+        LARGE,
+        LARGEST
+    )
+
+    fun ArrayList<Float>.getFontSizeByPosition(position: Int) = this[position]
 }
