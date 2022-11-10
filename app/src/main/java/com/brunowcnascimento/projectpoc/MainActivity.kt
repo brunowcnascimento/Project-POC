9aed6518412b9ee110c3e7cb46f5e5b063b734f7
Bruno Nascimento <bruno.nascimento@g.globo>
2022-11-10 15:52:35 -0300

    tirando os imports desnecessarios

--- app/src/main/java/com/brunowcnascimento/projectpoc/MainActivity.kt
+++ app/src/main/java/com/brunowcnascimento/projectpoc/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.brunowcnascimento.projectpoc
 
-import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
-import com.brunowcnascimento.projectpoc.utils.common.CommonGenericActivity
 import com.brunowcnascimento.projectpoc.databinding.ActivityMainBinding
 import com.brunowcnascimento.projectpoc.feature.font_size.FontSizeFontSizeActivity
-import com.brunowcnascimento.projectpoc.feature.font_size.setup.FontSizeManager
 import com.brunowcnascimento.projectpoc.setup_main.MainAdapter
 import com.brunowcnascimento.projectpoc.setup_main.MainRecyclerDomain
+import com.brunowcnascimento.projectpoc.utils.common.CommonGenericActivity
 
 class MainActivity : CommonGenericActivity() {
 
@@ -52,14 +46,11 @@ class MainActivity : CommonGenericActivity() {
     }
 
     private fun verificationActivity(name: String) {
-
         when(name) {
             FontSizeFontSizeActivity.newInstance().javaClass.name -> {
                 val intent = FontSizeFontSizeActivity.getIntent(this)
                 startActivity(intent)
             }
-//            else ->
-//                getToast("Activity não cadastrada").show()
         }
     }
 }
